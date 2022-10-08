82ac9144dc8ab43fdc95a22dc8a8216a4181ccf1
guillermolavagnino <113811618+guillermolavagnino@users.noreply.github.com>
2022-10-08 14:47:56 -0300

    Add files via upload

--- detail.php
+++ detail.php
@@ -194,10 +194,10 @@ require __DIR__ . '/vendor/autoload.php';
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo $_POST['price'] ?>
+                                            <?php echo "$" . $_POST['price'] ?>
                                         </h3>
                                         <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
+                                            Cantidad: <?php echo $_POST['unit'] ?>
                                         </h3>
                                     </div>
                                     <!--<button type="submit" class="mercadopago-button" formmethod="post">Pagar</button> -->
