3108dff156f4f9d3f62f83984040a1b49ed3834e
guillermolavagnino <113811618+guillermolavagnino@users.noreply.github.com>
2022-10-10 00:32:45 -0300

    Add files via upload

--- notificaciones.php
+++ notificaciones.php
@@ -27,57 +27,26 @@ MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
           break;
   }
   
-  //if (isset($payment) && !empty($payment)) {
-	//$json = file_get_contents($payment);
-	$json = file_get_contents("php://input");
-    //file_put_contents("https://glavagnino-mp-ecommerce-php.herokuapp.com/webhook.txt", $json . PHP_EOL);
-  //}
+
+	//$json = file_get_contents("php://input");
+    //file_put_contents("./webhook.txt", $json . PHP_EOL);
  
-  $wh = fopen( __DIR__ .'/webhook.txt', 'r+b');
-  //fwrite($wh, "WEBHOOK");
-  //$json = json_encode($json);
-  fwrite($wh, print_r($json, true));
-  //fwrite($wh, $json);
-  fclose($wh);
+  //$wh = fopen( __DIR__ .'/webhook.txt', 'r+b');
+  //////////$json = json_encode($json);
+  //fwrite($wh, print_r($json, true));
+  ///////////fwrite($wh, $json);
+  //fclose($wh);
+  
 
 
- 
-   $merchant_order = NULL;
- 
-   switch($_GET["topic"]) {
-       case "payment":
-           $payment_ipn = MercadoPago\Payment::find_by_id($_GET["id"]);
-           // Get the payment and the corresponding merchant_order reported by the IPN.
-           $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment_ipn->order->id);
-           break;
-       case "merchant_order":
-           $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
-           break;
-   }
- 
-   $paid_amount = 0;
-   foreach ($merchant_order->payments as $payment_ipn) {  
-       if ($payment_ipn['status'] == 'approved'){
-           $paid_amount += $payment_ipn['transaction_amount'];
-       }
-   }
- 
-   // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-   if($paid_amount >= $merchant_order->total_amount){
-       if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
-           if($merchant_order->shipments[0]->status == "ready_to_ship") {
-               $msj = "Totalmente pagado. Imprime la etiqueta y libera tu artículo.";
-			   file_put_contents("./ipn.txt", $msj);
-           }
-       } else { // The merchant_order don't has any shipments
-           $msj = "Totalmente pagado. Libera tu artículo.";
-		   file_put_contents("./ipn.txt", $msj);
-       }
-   } else {
-       $msj = "Aún no pagado. No liberes tu artículo.";
-	   file_put_contents("./ipn.txt", $msj);
-   }
+        $json = file_get_contents("php://input");
+		header_remove();
+        $code = '200 OK';
+        http_response_code($code);
+		header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+        header('Content-Type: application/json');
+		header('Status: 200 OK');
+        file_put_contents("./webhook.txt", $json);
 
-header("HTTP/1.1 200 OK");
 
 ?>
