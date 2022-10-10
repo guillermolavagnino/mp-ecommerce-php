9a15d96d3dac35d92b791d2f4b0997fa7a37ff21
guillermolavagnino <113811618+guillermolavagnino@users.noreply.github.com>
2022-10-10 03:33:22 -0300

    Add files via upload

--- notificaciones.php
+++ notificaciones.php
@@ -36,7 +36,7 @@ MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
   $wh = fopen( __DIR__ .'/webhook.txt', 'r+b');
   //fwrite($wh, "WEBHOOK");
   $json = json_decode($json, true);
-  $json = json_encode($json . PHP_EOL, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+  $json = json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
   fwrite($wh, $json);
   fclose($wh);
 
